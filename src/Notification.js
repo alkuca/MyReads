d573b53e848904d1973eb743ecdb23a8042eefed
Alen <alkuca@gmail.com>
2018-06-23 21:03:01 +0200

    changes

--- src/Notification.js
+++ src/Notification.js
@@ -3,9 +3,36 @@ import './App.css'
 
 class Notification extends Component{
     render(){
+        let shelfChooser = this.props.shelfTitle;
+        let youAdded = "You added the";
+        let toThe ="to the";
+        let shelf ="shelf";
+        let congratulations="";
+
+        if(shelfChooser === "read"){
+            shelfChooser = "Read";
+            congratulations = "Congratulations!"
+        }
+        if(shelfChooser === "currentlyReading"){
+            shelfChooser = "Currently Reading"
+        }
+        if(shelfChooser === "wantToRead"){
+            shelfChooser = "Want to Read"
+        }
+
+        if(shelfChooser === "none"){
+            youAdded = "You removed the";
+            toThe = "";
+            shelf = "";
+            shelfChooser="";
+        }
         return(
             <h4 className="notification-text">
-                You added <span className="book-notification-title">{this.props.bookTitle}</span> to the <span className="book-notification-shelf">{this.props.shelfTitle}</span> shelf
+                {congratulations} {youAdded}
+                <span className="book-notification-title"> {this.props.bookTitle} </span>
+                book {toThe}
+                <span className="book-notification-shelf"> {shelfChooser} </span>
+                {shelf}
             </h4>
         )
     }
